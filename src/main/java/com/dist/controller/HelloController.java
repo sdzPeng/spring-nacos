7e9e4302b1b8b41341934a7c6bee119eb78975ac
zhangyp <zyp19930326>
2020-03-06 01:46:30 +0800

    feat:test

--- src/main/java/com/dist/controller/HelloController.java
+++ src/main/java/com/dist/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.dist.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,11 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @desc：
  */
 @RestController
-@RequestMapping(value = "hello")
+@RequestMapping("hello")
 public class HelloController {
 
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private String useLocalCache;
+
     @GetMapping("/sayHello")
+    @ResponseBody
     public String sayHello() {
-        return "hello world";
+        System.out.println("test");
+        return useLocalCache;
     }
 }
